109b9c58364311a8013783c3420f589a924154ac
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-23 11:47:47 +0000

    Added useEffect() functions and examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
+
 function App() {
+  const [month, setMonth] = useState('');
+  const [priority, setPriority] = useState('');
+
+  
+  //Everytime there is a render useEffect is called. Avoids problems with asynchronisity.
+  useEffect(() => {
+    console.log(number);
+  });
+
+  useEffect(() => {
+    console.log('priority change');
+  }, [priority]); //add an array with variables that you want to cause useEffect to happen.
+
+  useEffect(() => { 
+     //Limits number of actions for a time frame.
+    const timer = setTimeout(() => {
+      console.log('DAYTIME TV MADE ME LESBIAN') 
+    }, 3000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [priority, month]); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h2>Month: {month}</h2>
+      <button onClick={() => setMonth('January')}>January </button>
+      <button onClick={() => setMonth('February')}>February </button>
+      <button onClick={() => setMonth('March')}>March </button>
     </div>
+    <div>
+      <h2>Priority: {priority}</h2>
+      <button onClick={() => setPriority('high')}>high</button>
+      <button onClick={() => setPriority('low')}>low</button>
+    </div>
+    </>
   );
+
+  
+
+ 
+
+
 }
 
 export default App;
+
+
+
+
+
+
+
